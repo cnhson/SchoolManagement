d70c21d089335833d0d85dc2111d74b7784d2e3f
Fox Nguyen <ssonpros@yahoo.com.vn>
2021-05-01 15:50:39 +0700

    ..

---
+++ WindowsFormsApp1/Add_Student.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Add_Student : Form
+    {
+        public Add_Student()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.Handled =! char.IsNumber(e.KeyChar) && e.KeyChar != 8)
+            {
+                errorProvider1.SetError(label9,"Only Number");
+                label9.Text = "Only Number";
+            }
+            else
+            {
+                errorProvider1.SetError(label9, "");
+                label9.Text = "";
+            }
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.Handled = !char.IsLetter(e.KeyChar) && e.KeyChar != 8)
+            {
+                errorProvider1.SetError(label10, "Only Letters");
+                label10.Text = "Only Letters";
+            }
+            else
+            {
+                errorProvider1.SetError(label10, "");
+                label10.Text = "";
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.Handled = !char.IsLetter(e.KeyChar) && e.KeyChar != 8)
+            {
+                errorProvider1.SetError(label11, "Only Letters");
+                label11.Text = "Only Letters";
+            }
+            else
+            {
+                errorProvider1.SetError(label11, "");
+                label11.Text = "";
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.Handled = !char.IsNumber(e.KeyChar) && e.KeyChar != 8)
+            {
+                errorProvider1.SetError(label12, "Only Number");
+                label12.Text = "Only Number";
+            }
+            else
+            {
+                errorProvider1.SetError(label12, "");
+                label12.Text = "";
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Student_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Uploadbtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg;*.jpeg;*.gif;*.bmp)|*.jpg;*.jpeg;*.gif;*.bmp;";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(open.FileName);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddSt_Click(object sender, EventArgs e)
+        {
+            Student st = new Student();
+            int id = Convert.ToInt32(id_Box.Text);
+            string fname = fname_Box.Text;
+            string lname = lname_Box.Text;
+            DateTime bdate = bdate_Box.Value;
+            string phone = phone_Box.Text;
+            string adrs = address_Box.Text;
+            string gender = "Male";
+            MemoryStream pic = new MemoryStream();
+
+            if (female_Box.Checked)
+            {
+                gender = "Female";
+            }
+
+            if (verif())
+            {
+                pictureBox1.Image.Save(pic, pictureBox1.Image.RawFormat);
+                if(st.addStudent(id,fname,lname,bdate,gender,phone,adrs,pic))
+                {
+                    MessageBox.Show("Add Student Successful","Add Student",MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("Error", "Add Student", MessageBoxButtons.OK);
+                }
+            }
+
+            bool verif()
+            {
+                if ((fname_Box.Text.Trim() == "")
+                    || (lname_Box.Text.Trim() == "")
+                    || (address_Box.Text.Trim() == "")
+                    || (phone_Box.Text.Trim() == "")
+                    || (pictureBox1.Image == null))
+                { 
+                    return false; 
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void id_Box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/List.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class List
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataDataSet = new WindowsFormsApp1.dataDataSet();
+            this.dataDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bdate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.picture = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Black;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.fname,
+            this.lname,
+            this.bdate,
+            this.gender,
+            this.phone,
+            this.address,
+            this.picture});
+            this.dataGridView1.DataSource = this.dataDataSetBindingSource;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, -3);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(857, 333);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(110, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "View";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataDataSet
+            // 
+            this.dataDataSet.DataSetName = "dataDataSet";
+            this.dataDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataDataSetBindingSource
+            // 
+            this.dataDataSetBindingSource.DataSource = this.dataDataSet;
+            this.dataDataSetBindingSource.Position = 0;
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "ID";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 90;
+            // 
+            // fname
+            // 
+            this.fname.DataPropertyName = "fname";
+            this.fname.HeaderText = "First Name";
+            this.fname.MinimumWidth = 6;
+            this.fname.Name = "fname";
+            this.fname.ReadOnly = true;
+            this.fname.Width = 125;
+            // 
+            // lname
+            // 
+            this.lname.DataPropertyName = "lname";
+            this.lname.HeaderText = "Last Name";
+            this.lname.MinimumWidth = 6;
+            this.lname.Name = "lname";
+            this.lname.ReadOnly = true;
+            this.lname.Width = 125;
+            // 
+            // bdate
+            // 
+            this.bdate.DataPropertyName = "bdate";
+            this.bdate.HeaderText = "Birthday";
+            this.bdate.MinimumWidth = 6;
+            this.bdate.Name = "bdate";
+            this.bdate.ReadOnly = true;
+            this.bdate.Width = 80;
+            // 
+            // gender
+            // 
+            this.gender.DataPropertyName = "gender";
+            this.gender.HeaderText = "Gender";
+            this.gender.MinimumWidth = 6;
+            this.gender.Name = "gender";
+            this.gender.ReadOnly = true;
+            this.gender.Width = 50;
+            // 
+            // phone
+            // 
+            this.phone.DataPropertyName = "phone";
+            this.phone.HeaderText = "Phone";
+            this.phone.MinimumWidth = 6;
+            this.phone.Name = "phone";
+            this.phone.ReadOnly = true;
+            this.phone.Width = 80;
+            // 
+            // address
+            // 
+            this.address.DataPropertyName = "address";
+            this.address.HeaderText = "Address";
+            this.address.MinimumWidth = 6;
+            this.address.Name = "address";
+            this.address.ReadOnly = true;
+            this.address.Width = 125;
+            // 
+            // picture
+            // 
+            this.picture.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.picture.DataPropertyName = "picture";
+            this.picture.HeaderText = "Image";
+            this.picture.MinimumWidth = 60;
+            this.picture.Name = "picture";
+            this.picture.ReadOnly = true;
+            this.picture.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.picture.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.picture.Width = 200;
+            // 
+            // List
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(850, 417);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "List";
+            this.Text = "DataView";
+            this.Load += new System.EventHandler(this.List_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource dataDataSetBindingSource;
+        private dataDataSet dataDataSet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bdate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.DataGridViewImageColumn picture;
+    }
+}
